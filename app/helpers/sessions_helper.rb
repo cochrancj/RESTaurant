3799140234c200d9dcb766232dcdab2481ab7390
Carolyn Cochran <cochran.cj@gmail.com>
2016-04-24 12:35:24 -0400

    deploying to Heroku - because I'm walking through it with another scared Noob

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,13 +1,4 @@
 module SessionsHelper
-  #
-  # before_filter :redirect_if_not_admin :only => [:root]
-  #
-  # def redirect_if_not_admin
-  #   if !@user.is_admin?
-  #   redirect_to :root
-  # end
-
-  # RETURNS ERROR COMPLAINING ABOUT SYNTAX - NOT EXPECTING :
 
   def current_server
     @current_server ||= get_current_server
